e35150c27af653a65d01708582b2b94d95dae803
Cmonroyz <carlosdavid.monroyzapata@emeal.nttdata.com>
2025-07-16 14:45:05 +0200

    Implementacion de los metodos de update y deleted con nuestros repositorios.

--- src/presenation/todos/controller.ts
+++ src/presenation/todos/controller.ts
@@ -62,40 +62,15 @@ export class TodosController {
   public updateTodo = async (req: Request, res: Response) => {
     const id = +req.params.id;
     const [error, updateTodoDto] = UpdateTodoDto.create({ ...req.body, id });
-    if (error) {
-      res.status(400).json({ error });
-      return;
-    }
+    if (error) return res.status(400).json({ error });
 
-    const todo = await prisma.todo.findFirst({
-      where: { id }
-    });
-    if (!todo) {
-      res.status(404).json({ error: `Todo with id ${id} not found` });
-      return;
-    }
-
-    const updatedTodo = await prisma.todo.update({
-      where: { id },
-      data: updateTodoDto!.values
-    });
-    res.json(updatedTodo);
+    const updateTodo = await this.todoRepository.updateById(updateTodoDto!);
+    return res.json(updateTodo)
   };
 
   public deleteTodo = async (req: Request, res: Response) => {
     const id = +req.params.id;
-
-    const todo = await prisma.todo.findFirst({
-      where: { id }
-    });
-    if (!todo) {
-      res.status(404).json({ error: `Todo with id ${id} not found` });
-      return;
-    }
-
-    const deleted = await prisma.todo.delete({
-      where: { id }
-    });
-    res.json(deleted);
+    const deletedTodo = await this.todoRepository.deleteById(id);
+    return res.json(deletedTodo);
   };
 }
